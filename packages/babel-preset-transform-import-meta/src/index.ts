0ab34652b059b5d1c96563c6ea086ec9e6440ff7
yangzhiganlu-fav <3076383545@qq.com>
2024-07-16 00:35:27 +0800

    ✨ feat: 新增preset

---
+++ packages/babel-preset-transform-import-meta/src/index.ts
@@ -0,0 +1,22 @@
+import babelCore, { ConfigItem } from '@babel/core';
+import envPlugin, { PluginOpts as EnvPluginOpts } from 'babel-plugin-transform-import-meta-env'
+
+export interface PresetOptions {
+    env?: boolean | EnvPluginOpts
+}
+
+const preset = (_: typeof babelCore, opts: PresetOptions): { plugins: ConfigItem[] } => {
+    const plugins: ConfigItem[] = [];
+
+    if (opts.env === true || opts.env === undefined) {
+        plugins.push(_.createConfigItem(envPlugin));
+    } else if (typeof opts.env === 'object') {
+        plugins.push(_.createConfigItem([envPlugin, opts.env]));
+    }
+
+    return {
+        plugins,
+    };
+}
+
+export default preset;
