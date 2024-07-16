c3a2434cb99d0792f47e9052cc323bf4f6841f69
yangheng <3076383545@qq.com>
2024-07-16 14:58:09 +0800

    ✨ feat: preset新增glob插件

--- packages/babel-preset-transform-import-meta/__tests__/index.test.ts
+++ packages/babel-preset-transform-import-meta/__tests__/index.test.ts
@@ -1,32 +1,82 @@
 import preset from "../src/index";
 import babelCore from "@babel/core";
 import envPlugin from "babel-plugin-transform-import-meta-env";
-import { resolve } from "path";
+import globPlugin from "babel-plugin-transform-import-meta-glob";
 
 describe("babel-preset-transform-import-meta", () => {
+    it('should call preset function with default options', () => {
+        const result = preset(babelCore);
+        expect(result).toHaveProperty('plugins');
+        expect(result.plugins).toBeInstanceOf(Array);
+    });
+
     it("should return a preset object", () => {
-        const opts = { env: true };
+        const opts = { env: true, glob: true };
         const result = preset(babelCore, opts);
-        expect(result).toEqual({ plugins: [babelCore.createConfigItem(envPlugin)] });
+        expect(result).toEqual({
+            plugins: [
+                babelCore.createConfigItem(envPlugin),
+                babelCore.createConfigItem(globPlugin),
+            ],
+        });
     });
-    it("should not return a preset object", () => {
-        const opts = { env: false };
-        const result = preset(babelCore, opts);
-        expect(result).toEqual({ plugins: [] });
-    });
+
     it("should return a preset object with empty option", () => {
         const opts = {};
         const result = preset(babelCore, opts);
-        expect(result).toEqual({ plugins: [babelCore.createConfigItem(envPlugin)] });
+        expect(result).toEqual({
+            plugins: [
+                babelCore.createConfigItem(envPlugin),
+                babelCore.createConfigItem(globPlugin),
+            ],
+        });
     });
-    it("should return a preset object with mockData options", () => {
-        const opts = { env: { mockData: { VITE_APP_NAME: "bar" } } };
+
+    it("should return a preset object with env plugin", () => {
+        const opts = { env: true, glob: false };
         const result = preset(babelCore, opts);
-        expect(result).toEqual({ plugins: [babelCore.createConfigItem([envPlugin, opts.env])] });
+        expect(result).toEqual({
+            plugins: [
+                babelCore.createConfigItem(envPlugin),
+            ],
+        });
     });
-    it("should return a preset object with envFile options", () => {
-        const opts = { env: { envFile: resolve(process.cwd(), './fixtures/.env') } };
+
+    it("should return a preset object with glob options", () => {
+        const opts = { glob: true, env: false };
         const result = preset(babelCore, opts);
-        expect(result).toEqual({ plugins: [babelCore.createConfigItem([envPlugin, opts.env])] });
+        expect(result).toEqual({
+            plugins: [
+                babelCore.createConfigItem(globPlugin),
+            ],
+        });
+    });
+
+    it("should return a preset object with glob options", () => {
+        const opts = { glob: false, env: false };
+        const result = preset(babelCore, opts);
+        expect(result).toEqual({
+            plugins: [],
+        });
+    });
+
+    it("should return a preset object with env plugin with options", () => {
+        const opts = { env: { mockData: { VITE_XXX: '123' } }, glob: false };
+        const result = preset(babelCore, opts);
+        expect(result).toEqual({
+            plugins: [
+                babelCore.createConfigItem([envPlugin, opts.env]),
+            ],
+        });
+    });
+
+    it("should return a preset object with glob options with options", () => {
+        const opts = { glob: { eager: true }, env: false };
+        const result = preset(babelCore, opts);
+        expect(result).toEqual({
+            plugins: [
+                babelCore.createConfigItem([globPlugin, opts.glob]),
+            ],
+        });
     });
 });
--- packages/babel-preset-transform-import-meta/src/index.ts
+++ packages/babel-preset-transform-import-meta/src/index.ts
@@ -1,18 +1,32 @@
 import babelCore, { ConfigItem } from '@babel/core';
 import envPlugin from 'babel-plugin-transform-import-meta-env'
 import type { PluginOpts as EnvPluginOpts } from 'babel-plugin-transform-import-meta-env'
+import globPlugin from 'babel-plugin-transform-import-meta-glob'
+import type { PluginOpts as GlobPluginOpts } from 'babel-plugin-transform-import-meta-glob'
 
 export interface PresetOptions {
     env?: boolean | EnvPluginOpts
+    glob?: boolean | GlobPluginOpts
 }
 
-const preset = (_: typeof babelCore, opts: PresetOptions): { plugins: ConfigItem[] } => {
+const defaultOpts: PresetOptions = {
+    env: true,
+    glob: true,
+}
+
+const preset = (_: typeof babelCore, opts: PresetOptions = defaultOpts): { plugins: ConfigItem[] } => {
     const plugins: ConfigItem[] = [];
 
     if (opts.env === true || opts.env === undefined) {
         plugins.push(_.createConfigItem(envPlugin));
     } else if (typeof opts.env === 'object') {
         plugins.push(_.createConfigItem([envPlugin, opts.env]));
+    }
+
+    if (opts.glob === true || opts.glob === undefined) {
+        plugins.push(_.createConfigItem(globPlugin));
+    } else if (typeof opts.glob === 'object') {
+        plugins.push(_.createConfigItem([globPlugin, opts.glob]));
     }
 
     return {
